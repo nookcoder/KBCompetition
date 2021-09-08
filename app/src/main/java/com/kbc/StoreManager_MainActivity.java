a795b2782c2e7db11936cdae5aed07d9792ec089
seohee0112 <ghdtj7@gmail.com>
2021-09-09 02:01:49 +0900

    [ADD] 하단바 소상공인 메인 엑티비티에 연결

--- app/src/main/java/com/kbc/StoreManager_MainActivity.java
+++ app/src/main/java/com/kbc/StoreManager_MainActivity.java
@@ -1,5 +1,57 @@
 package com.kbc;
 
-public class StoreManager_MainActivity {
+import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class StoreManager_MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.storemanager_activity_main);
+
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.commit();
+
+        fragmentTransaction.addToBackStack(null);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.store_manger_bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new bottomMenuSelectListener());
+    }
+
+    class bottomMenuSelectListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()){
+                case R.id.nav_sale:
+
+                    break;
+
+                case R.id.nav_chatting:
+
+                    break;
+
+                case R.id.nav_information:
+
+                    break;
+            }
+
+            return true;
+        }
+
+    }
 }
