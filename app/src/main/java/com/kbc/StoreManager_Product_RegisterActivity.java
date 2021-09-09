51ea574f7bfefcce92f74483b8f090104631e5e4
seohee0112 <ghdtj7@gmail.com>
2021-09-09 22:38:56 +0900

    [ADD] 등록하기 액티비티 및 툴바 + 하단 등록 버튼 추가

---
+++ app/src/main/java/com/kbc/StoreManager_Product_RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.kbc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreManager_Product_RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_manager_product_register);
+    }
+}
