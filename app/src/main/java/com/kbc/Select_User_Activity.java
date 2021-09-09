e833769df0bbd098ec0b6e5b938777394d97f5a9
hyunjin <jhj981102@gmail.com>
2021-09-09 17:01:43 +0900

    [remove]기본 툴바 삭제

--- app/src/main/java/com/kbc/Select_User_Activity.java
+++ app/src/main/java/com/kbc/Select_User_Activity.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Select_Log_Activity extends AppCompatActivity {
+public class Select_User_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.select_log_activity);
+        setContentView(R.layout.select_user_activity);
     }
 }
