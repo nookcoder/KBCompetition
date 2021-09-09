0eefcf8b93bb9378c249dd8e29b670c5fed2c98d
hyunjin <jhj981102@gmail.com>
2021-09-09 17:14:17 +0900

    [add]화면 전환 추가 , 로그 데이터 전달

--- app/src/main/java/com/kbc/Select_Log_Activity.java
+++ app/src/main/java/com/kbc/Select_Log_Activity.java
@@ -1,6 +1,9 @@
 package com.kbc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +11,32 @@ public class Select_Log_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //화면 셋팅
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_log_activity);
+
+        //회원가입 버튼에 리스너 달기
+        Button signupButton = (Button) findViewById(R.id.signup);
+        signupButton.setOnClickListener(new View.OnClickListener(){
+          @Override
+          public void onClick(View view){
+              //화면 전환 + 로그 전달
+              Intent intent = new Intent(getApplicationContext(),Select_User_Activity.class);
+              intent.putExtra("log","sign up");
+              startActivity(intent);
+          }
+        });
+
+        //로그인 버튼에 리스너 달기
+        Button signinButton = (Button) findViewById(R.id.signin);
+        signupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //화면 전환 + 로그 전달
+                Intent intent = new Intent(getApplicationContext(),Select_User_Activity.class);
+                intent.putExtra("log","signin up");
+                startActivity(intent);
+            }
+        });
     }
 }
