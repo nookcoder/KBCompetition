6bd7ae609f1192396645c31115815d9718fa84fe
hyunjin <jhj981102@gmail.com>
2021-09-12 21:30:04 +0900

    [add]라디오그룹 선택하면 버튼 색 변경,,

--- app/src/main/java/com/kbc/Login.java
+++ app/src/main/java/com/kbc/Login.java
@@ -1,8 +1,17 @@
 package com.kbc;
 
+import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +19,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
+        // 라디오그룹 참조
+        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radiogroup);
+        Button login = (Button)findViewById(R.id.loginButton);
+        // 클릭이벤트
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                RadioButton select = (RadioButton)findViewById(id);
+                login.setTextColor(Color.parseColor("#FFC11B"));
+            }
+        });
     }
 }
