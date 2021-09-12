b953b2842684bb583a0323c74da4dbcc7dc63964
hyunjin <jhj981102@gmail.com>
2021-09-12 23:57:41 +0900

    [add]버튼에 텍스트 크기 다르게 조절 메소드 추가

--- app/src/main/java/com/kbc/Login.java
+++ app/src/main/java/com/kbc/Login.java
@@ -3,7 +3,14 @@ package com.kbc;
 import android.content.Intent;
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,34 +27,40 @@ import com.kakao.sdk.user.model.Account;
 public class Login extends AppCompatActivity {
     String selectedUser;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        Intent intent1 = new Intent(Login.this, StoreManager_Chatting_Send_Activity.class);
-        Intent intent2 = new Intent(Login.this, Select_Log_Activity.class);
 
-        //테스트
-        TextView textView = (TextView) findViewById(R.id.textView);
+        //화면 전환 2개!
+        Intent intent1 = new Intent(Login.this, StoreManager_Chatting_Send_Activity.class);//변경 필요!!!
+        Intent intent2 = new Intent(Login.this, Select_Log_Activity.class);//변경 필요!!!
 
         // 라디오그룹 참조
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
+        RadioButton store = (RadioButton) findViewById(R.id.storeManagerSelected);
+        RadioButton person = (RadioButton) findViewById(R.id.personSelected);
+        //텍스트 크기 줄마다 다르게 조절
+        makeOtherSize(store,"음식을 팔고 싶어요.");
+        makeOtherSize(person,"음식을 사고 싶어요.");
+
         Button login = (Button) findViewById(R.id.loginButton);
         login.setEnabled(false);
 
-        // 클릭이벤트
+        // 라디오 그룹에 이벤트
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int id) {
                 login.setEnabled(true);
                 RadioButton select = (RadioButton) findViewById(id);
                 if (id == R.id.storeManagerSelected) {
-                    textView.setText("사업자");
                     selectedUser = "사업자";
                 } else if (id == R.id.personSelected) {
-                    textView.setText("개인");
                     selectedUser = "개인";
                 }
+                //하나라도 선택되면 버튼 색 변경
                 login.setTextColor(Color.parseColor("#FFC11B"));
             }
         });
@@ -56,11 +69,10 @@ public class Login extends AppCompatActivity {
         Button loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(v -> UserApiClient.getInstance().loginWithKakaoTalk(Login.this, (oAuthToken, error) -> {
             if (error != null) {
-                Log.e("으앙", "로그인 실패", error);
+                Log.e("으악", "로그인 실패", error);
                 startActivity(intent1);
-
             } else if (oAuthToken != null) {
-                Log.i("으악", "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
+                Log.i("우왕", "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
 
                 UserApiClient.getInstance().me((user, meError) -> {
                     if (meError != null) {
@@ -76,19 +88,31 @@ public class Login extends AppCompatActivity {
                         Account user1 = user.getKakaoAccount();
                         System.out.println("사용자 계정" + user1);
                     }
+
+                    //사업자 화면 전환!!
                     if(selectedUser.equals("사업자")) {
                         startActivity(intent1);
                     }
+                    //개인 화면 전환!!
                     else if(selectedUser.equals("개인")) {
                         startActivity(intent2);
                     }
-                    textView.setText(selectedUser.toString());
-
                     return null;
                 });
             }
             return null;
         }));
+    }
+
+    //텍스트 특정 글자 크기 다르게 하기
+    public void makeOtherSize(RadioButton radioButton , String word){
+        String content = radioButton.getText().toString();
+        SpannableString spannableString = new SpannableString(content);
+        int start = content.indexOf(word);
+        int end=start+word.length();
+        spannableString.setSpan(new StyleSpan(Typeface.NORMAL), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new RelativeSizeSpan(0.6f), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        radioButton.setText(spannableString);
     }
 }
 
