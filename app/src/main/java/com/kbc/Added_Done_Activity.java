90a2524f2ed48162345ce2fd126a193528f6045b
hyunjin <jhj981102@gmail.com>
2021-09-13 15:49:21 +0900

    [add]정보 등록 완료 UI+user 데이터 받기

---
+++ app/src/main/java/com/kbc/Added_Done_Activity.java
@@ -0,0 +1,39 @@
+package com.kbc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//이전 intent 에서 유저가 누군지 받아와야됨!
+public class Added_Done_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.go_to_start);
+
+        //유저 데이터 받기
+        Intent intentForGet = getIntent();
+        String name = intentForGet.getExtras().getString("user");
+
+        //다음 버튼
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //유저가 사업자면
+                if(name.equals("store manager")){
+                    Intent intent1 = new Intent(getApplicationContext(), StoreManager_Chatting_Send_Activity.class);//변경 필요
+                    startActivity(intent1);
+                }
+                //유저가 개인이면
+                else if(name.equals("person")){
+                    Intent intent2 = new Intent(getApplicationContext(), StoreManager_Chatting_Send_Activity.class);//변경 필요
+                    startActivity(intent2);
+                }
+            }
+        });
+    }
+}
