261a947ffe2c9c12be78922042d933cb24c31d97
seohee0112 <ghdtj7@gmail.com>
2021-09-14 01:29:42 +0900

    [ADD] 리스트 아이템 xml 추가

---
+++ app/src/main/java/com/kbc/Chatting/Chatting_List_RecycleAdapter.java
@@ -0,0 +1,29 @@
+package com.kbc.Chatting;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.kbc.Chatting.Chatting_List_ViewHolder;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Chatting_List_RecycleAdapter extends RecyclerView.Adapter<Chatting_List_RecycleAdapter.Chatting_List_ViewHolder> {
+    @NonNull
+    @NotNull
+    @Override
+    public Chatting_List_RecycleAdapter onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull Chatting_List_RecycleAdapter holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
