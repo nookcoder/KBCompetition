675ef09a1a8e6ac9e64ec055d70a4dbcc08f3476
seohee0112 <ghdtj7@gmail.com>
2021-09-13 18:45:52 +0900

    [ADD] 점주 아이디 별로 채팅방 내역 + 채팅 내용 불러오기 코드 추가

--- app/src/main/java/com/kbc/FirebaseConnector.java
+++ app/src/main/java/com/kbc/FirebaseConnector.java
@@ -10,6 +10,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import static android.content.ContentValues.TAG;
@@ -50,7 +52,9 @@ public class FirebaseConnector {
         }
     }
 
-    private Map<String, Object> map, id_map, chatrooms_map, chatting_map;
+    private Map<String, Object> map, id_map,id_inside_map;
+    private ArrayList<HashMap<String, String>> chatrooms_arraylist, chatting_arraylist;
+
 
 
     //정보 가져오기
@@ -67,18 +71,41 @@ public class FirebaseConnector {
                         Log.d(TAG, "카카오아이디 : "+ id_map);
                     }
 
+                    Log.d(TAG, "아이디 키 : " +id_map.keySet());
 
-                    //아이디 -> 채팅방 맵에 담기
-                    for(String chatrooms: id_map.keySet()){
-                        chatrooms_map = (Map<String, Object>)id_map.get(chatrooms);
-                        Log.d(TAG, "채팅방 내역 : " + chatrooms_map);
+                    for(String id_inside : id_map.keySet()){
+                        id_inside_map = (Map<String, Object>)id_map.get(id_inside);
+                        Log.d(TAG, "아이디 내부 : "+ id_inside_map);
+
                     }
-                    //아이디 -> 채팅내용 맵에 담기
-                    for(String chatting: id_map.keySet()){
-                        chatting_map = (Map<String, Object>)id_map.get(chatting);
-                        Log.d(TAG, "채팅방 내용 : " + chatting_map);
+
+                    Log.d(TAG, "아이디 내부 키 : " +id_inside_map.keySet());
+
+
+                    for(String in_inside_key: id_inside_map.keySet()){
+                        Log.d(TAG, "키 -> " + in_inside_key );
+
+
+                        switch (in_inside_key){
+                            //아이디 -> 채팅방 리스트에 담기
+                            case "chatrooms":
+                                chatrooms_arraylist = (ArrayList<HashMap<String, String>>)id_inside_map.get(in_inside_key);
+
+                                break;
+                            //아이디 -> 채팅 내역 리스트에 담기
+                            case "chatting":
+                                chatting_arraylist = (ArrayList<HashMap<String, String>>)id_inside_map.get(in_inside_key);
+                                break;
+                        }
+
+
                     }
+
                 }
+
+                Log.d(TAG, "채팅방 내역 : " + chatrooms_arraylist);
+                Log.d(TAG, "채팅방 사이즈 : " + chatrooms_arraylist.size());
+                Log.d(TAG, "채팅방 내용 : " + chatting_arraylist);
                 Log.d(TAG, "데이터 : " + map);
             }
 
