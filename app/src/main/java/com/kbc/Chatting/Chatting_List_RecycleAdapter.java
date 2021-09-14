4de6e2c43bdc6f3a6cde5c9b57a27e02f1c4ab30
seohee0112 <ghdtj7@gmail.com>
2021-09-14 14:12:02 +0900

    [ADD] 리스트 아이템 xml 채팅방 목록 추가

--- app/src/main/java/com/kbc/Chatting/Chatting_List_RecycleAdapter.java
+++ app/src/main/java/com/kbc/Chatting/Chatting_List_RecycleAdapter.java
@@ -1,29 +1,58 @@
 package com.kbc.Chatting;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import com.kbc.Chatting.Chatting_List_ViewHolder;
+
+import com.kbc.R;
 
 import org.jetbrains.annotations.NotNull;
 
-public class Chatting_List_RecycleAdapter extends RecyclerView.Adapter<Chatting_List_RecycleAdapter.Chatting_List_ViewHolder> {
+import java.util.ArrayList;
+
+public class Chatting_List_RecycleAdapter extends RecyclerView.Adapter<Chatting_List_RecycleAdapter.ViewHolder> {
+
+    //채팅방 데이터 추가
+    private ArrayList<Chatting_Item> chatting_items = null;
+
+    public Chatting_List_RecycleAdapter(ArrayList<Chatting_Item> chattingItems){
+        chatting_items = chattingItems;
+    }
+
+    //아이템 뷰를 위한 뷰홀더 객체를 형성 -> 레이아웃 생성
     @NonNull
-    @NotNull
     @Override
-    public Chatting_List_RecycleAdapter onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.chatting_list_item, viewGroup,false);
+        Chatting_List_RecycleAdapter.ViewHolder viewHolder = new Chatting_List_RecycleAdapter.ViewHolder(view);
+
+        return viewHolder;
     }
 
+    //뷰홀더의 position에 해당하는 데이터를 뷰홀더의 아이템 뷰에 표시
     @Override
-    public void onBindViewHolder(@NonNull @NotNull Chatting_List_RecycleAdapter holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.
     }
 
-    @Override
+    //아이템 개수 조회
+   @Override
     public int getItemCount() {
         return 0;
     }
 
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+        }
+    }
 }
