1a67a3346b92115c06a0c5b3744352d5258b75d2
seohee0112 <ghdtj7@gmail.com>
2021-09-14 15:27:41 +0900

    [ADD] Adapter추가

--- app/src/main/java/com/kbc/Chatting/Chatting_List_Fragment.java
+++ app/src/main/java/com/kbc/Chatting/Chatting_List_Fragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,11 +20,20 @@ import com.kbc.FirebaseConnector;
 import com.kbc.R;
 import com.kbc.StoreManager_MainActivity;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import static android.content.ContentValues.TAG;
 
 public class Chatting_List_Fragment extends Fragment  {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private ArrayList<Chatting_Item> chatting_items;
+    private Chatting_List_RecycleAdapter chatting_list_recycleAdapter;
+    private Chatting_Item chatting_item = new Chatting_Item();
+    private Chatting_Item chatting_item2 = new Chatting_Item();
+
 
     private StoreManager_MainActivity storeManager_mainActivity;
     private FirebaseConnector dbconnector;
@@ -30,19 +41,38 @@ public class Chatting_List_Fragment extends Fragment  {
     private String storeManager_id;
     private Bundle bundle;
 
+    //페이지 불러올떄마다 추가
     @Override
     public void onStart() {
         super.onStart();
 
+        //테스트용
+        chatting_item.setMessage("들어가쥬랑 ㅠㅡㅠ");
+        chatting_item.setName("서히");
+        chatting_item.setTime("01:12");
 
+
+
+        chatting_items.add(chatting_item);
+
+        chatting_item2.setMessage("제바류ㅠㅠㅠ");
+        chatting_item2.setName("현욱");
+        chatting_item2.setTime("05:16");
+
+        chatting_items.add(chatting_item2);
+        Log.d(TAG, "아이템 개수-- : "+  chatting_items.size());
+
+
+        chatting_list_recycleAdapter.notifyDataSetChanged();
     }
 
-    //뷰 그룹 반환
+    //뷰 그룹 반환 + 생성자 개념
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         ViewGroup rootViewGroup =(ViewGroup) inflater.inflate(R.layout.chatting_list, container, false);
+
 
         bundle = getArguments();
         if(bundle != null){
@@ -56,15 +86,17 @@ public class Chatting_List_Fragment extends Fragment  {
         dbconnector = FirebaseConnector.getInstance(storeManager_mainActivity, "StoreManager",storeManager_id);
 
 
+        //RecycleView 연결
+        recyclerView =rootViewGroup.findViewById(R.id.chatting_list_recycleView);
+        linearLayoutManager = new LinearLayoutManager(storeManager_mainActivity);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //RecycleView에 사용 Adapter생성
+        chatting_items = new ArrayList<>();
+        chatting_list_recycleAdapter = new Chatting_List_RecycleAdapter(chatting_items);
+        recyclerView.setAdapter(chatting_list_recycleAdapter);
+
         return rootViewGroup;
-    }
-
-    public void onClick(View view){
-        switch (view.getId()){
-            case R.id.send_message:
-                break;
-
-        }
     }
 }
 
--- app/src/main/java/com/kbc/Chatting/Chatting_List_RecycleAdapter.java
+++ app/src/main/java/com/kbc/Chatting/Chatting_List_RecycleAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,13 +15,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Chatting_List_RecycleAdapter extends RecyclerView.Adapter<Chatting_List_RecycleAdapter.ViewHolder> {
 
     //채팅방 데이터 추가
-    private ArrayList<Chatting_Item> chatting_items = null;
+    private ArrayList<Chatting_Item> chatting_items;
 
-    public Chatting_List_RecycleAdapter(ArrayList<Chatting_Item> chattingItems){
-        chatting_items = chattingItems;
+    public Chatting_List_RecycleAdapter(ArrayList<Chatting_Item> chatting_items){
+        this.chatting_items = chatting_items;
     }
 
     //아이템 뷰를 위한 뷰홀더 객체를 형성 -> 레이아웃 생성
@@ -38,21 +41,40 @@ public class Chatting_List_RecycleAdapter extends RecyclerView.Adapter<Chatting_
     //뷰홀더의 position에 해당하는 데이터를 뷰홀더의 아이템 뷰에 표시
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.
+//        holder.chatting_list_image_profil.setImageURI(chatting_items.get(position).getProfileUrl()));
+        holder.chatting_list_name.setText(chatting_items.get(position).getName());
+        holder.chatting_list_send_message.setText(chatting_items.get(position).getMessage());
+        holder.chatting_list_send_message_time.setText(chatting_items.get(position).getTime());
+
+        holder.itemView.setTag(position);
     }
 
     //아이템 개수 조회
    @Override
     public int getItemCount() {
-        return 0;
+        return chatting_items.size();
     }
 
 
+    //요소부르고
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        //프로필 사진, 이름, 메세지, 보낸 시간
+        protected CircleImageView chatting_list_image_profil;
+        protected TextView chatting_list_name;
+        protected TextView chatting_list_send_message;
+        protected TextView chatting_list_send_message_time;
 
 
         public ViewHolder(@NonNull @NotNull View itemView) {
+
             super(itemView);
+
+            this.chatting_list_image_profil = (CircleImageView)itemView.findViewById(R.id.chatting_circle_imageview);
+            this.chatting_list_name = (TextView)itemView.findViewById(R.id.chatting_list_name);
+            this.chatting_list_send_message = (TextView)itemView.findViewById(R.id.chatting_list_send_message);
+            this.chatting_list_send_message_time = (TextView)itemView.findViewById(R.id.chatting_list_send_message_time);
+
         }
     }
 }
