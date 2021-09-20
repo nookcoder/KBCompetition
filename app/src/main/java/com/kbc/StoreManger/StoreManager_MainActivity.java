f9fc4a6851de40b45130609d74dfea8523e3d5a2
seohee0112 <ghdtj7@gmail.com>
2021-09-20 20:05:41 +0900

    [ADD] 카메라 촬영 업롣 기능 추가

--- app/src/main/java/com/kbc/StoreManger/StoreManager_MainActivity.java
+++ app/src/main/java/com/kbc/StoreManger/StoreManager_MainActivity.java
@@ -17,7 +17,7 @@ import com.kbc.Chatting.Chatting_List_Fragment;
 import com.kbc.R;
 import com.kbc.Sale.StoreManager_Product_Register_Activity;
 import com.kbc.Sale.StoreManager_SalesList_Fragment;
-import com.kbc.StoreManager_Information_Fragment;
+import com.kbc.StoreManger.StoreManager_Information_Fragment;
 
 public class StoreManager_MainActivity extends AppCompatActivity {
 
