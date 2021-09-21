0f542093ca94f8367b33a35898cb9109693d792e
kim <gus5427@naver.com>
2021-09-21 16:38:21 +0900

    [ADD] 서버에 이미지 저장 기능 추가

---
+++ app/src/main/java/com/kbc/Image/ServiceApi.java
@@ -0,0 +1,17 @@
+package com.kbc.Image;
+
+import androidx.lifecycle.MutableLiveData;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface ServiceApi {
+    @Multipart
+    @POST("/product/img")
+    Call<ResponseBody> postImage(@Part MultipartBody.Part image, @Part("uploads") RequestBody name);
+}
