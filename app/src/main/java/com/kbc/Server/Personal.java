928c02db33cb8296f8205870e6f37597a0e10cfd
kim <gus5427@naver.com>
2021-09-23 17:35:17 +0900

    [ADD] Get townPosition 1,2

--- app/src/main/java/com/kbc/Server/Personal.java
+++ app/src/main/java/com/kbc/Server/Personal.java
@@ -22,4 +22,84 @@ public class Personal {
         this.townPosition1 = townPosition1;
         this.townPosition2 = townPosition2;
     }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getFirstLocation() {
+        return firstLocation;
+    }
+
+    public void setFirstLocation(String firstLocation) {
+        this.firstLocation = firstLocation;
+    }
+
+    public String getSecondLocation() {
+        return secondLocation;
+    }
+
+    public void setSecondLocation(String secondLocation) {
+        this.secondLocation = secondLocation;
+    }
+
+    public Boolean getRegister() {
+        return isRegister;
+    }
+
+    public void setRegister(Boolean register) {
+        isRegister = register;
+    }
+
+    public String getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(String registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    public int getTownPosition1() {
+        return townPosition1;
+    }
+
+    public void setTownPosition1(int townPosition1) {
+        this.townPosition1 = townPosition1;
+    }
+
+    public int getTownPosition2() {
+        return townPosition2;
+    }
+
+    public void setTownPosition2(int townPosition2) {
+        this.townPosition2 = townPosition2;
+    }
 }
