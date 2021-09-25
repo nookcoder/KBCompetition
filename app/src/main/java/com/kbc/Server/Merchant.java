3f340d1210bcfa17615c3af2e258369db99d1b2d
kim <gus5427@naver.com>
2021-09-25 18:49:05 +0900

    [ADD] 채팅 가게이름 받아오는 함수 추가

---
+++ app/src/main/java/com/kbc/Server/Merchant.java
@@ -0,0 +1,103 @@
+package com.kbc.Server;
+
+public class Merchant {
+    String id;
+    String storeName;
+    String storePhoneNumber;
+    String representativeName;
+    String openingDate;
+    String location;
+    String name;
+    String businessNumber;
+    int isRegister;
+    String town1;
+    String town2;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStorePhoneNumber() {
+        return storePhoneNumber;
+    }
+
+    public void setStorePhoneNumber(String storePhoneNumber) {
+        this.storePhoneNumber = storePhoneNumber;
+    }
+
+    public String getRepresentativeName() {
+        return representativeName;
+    }
+
+    public void setRepresentativeName(String representativeName) {
+        this.representativeName = representativeName;
+    }
+
+    public String getOpeningDate() {
+        return openingDate;
+    }
+
+    public void setOpeningDate(String openingDate) {
+        this.openingDate = openingDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBusinessNumber() {
+        return businessNumber;
+    }
+
+    public void setBusinessNumber(String businessNumber) {
+        this.businessNumber = businessNumber;
+    }
+
+    public int getIsRegister() {
+        return isRegister;
+    }
+
+    public void setIsRegister(int isRegister) {
+        this.isRegister = isRegister;
+    }
+
+    public String getTown1() {
+        return town1;
+    }
+
+    public void setTown1(String town1) {
+        this.town1 = town1;
+    }
+
+    public String getTown2() {
+        return town2;
+    }
+
+    public void setTown2(String town2) {
+        this.town2 = town2;
+    }
+}
