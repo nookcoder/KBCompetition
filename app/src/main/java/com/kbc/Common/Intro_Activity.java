589a1f0909ae09c84b55f5f5229dc2db09012845
kim <gus5427@naver.com>
2021-09-26 13:55:39 +0900

    [ADD] 로딩화면 추가

--- app/src/main/java/com/kbc/Common/Intro_Activity.java
+++ app/src/main/java/com/kbc/Common/Intro_Activity.java
@@ -3,10 +3,13 @@ package com.kbc.Common;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.kbc.Image.Image;
 import com.kbc.R;
+import com.squareup.picasso.Picasso;
 
 //인트로 화면
 public class Intro_Activity extends AppCompatActivity {
@@ -24,6 +27,12 @@ public class Intro_Activity extends AppCompatActivity {
                 finish();
             }
         },2000);
+        ImageView imageView = findViewById(R.id.logo);
+        Picasso.get()
+                .load(R.drawable.app_logo)
+                .centerInside()
+                .resize(1000,1000)
+                .into(imageView);
     }
 
     @Override
