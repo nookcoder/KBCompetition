1ea9698d7da5578b0ae9557844d8a093d7042e5e
hyunjin <jhj981102@gmail.com>
2021-09-26 16:27:18 +0900

    [add]인트로 애니메이션

--- app/src/main/java/com/kbc/Common/Intro_Activity.java
+++ app/src/main/java/com/kbc/Common/Intro_Activity.java
@@ -3,21 +3,27 @@ package com.kbc.Common;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.kbc.Image.Image;
 import com.kbc.R;
-import com.squareup.picasso.Picasso;
-
 //인트로 화면
 public class Intro_Activity extends AppCompatActivity {
 
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
+
+        image = (ImageView) findViewById(R.id.logo);
+        Animation ani = AnimationUtils.loadAnimation(getApplicationContext() , R.anim.intro);
+        image.startAnimation(ani);
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -26,13 +32,7 @@ public class Intro_Activity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
-        ImageView imageView = findViewById(R.id.logo);
-        Picasso.get()
-                .load(R.drawable.app_logo)
-                .centerInside()
-                .resize(1000,1000)
-                .into(imageView);
+        },3000);
     }
 
     @Override
